7cf8ae429fa77a2e9f35c3843e397bda1cda4d39
MH <436077853@qq.com>
2020-05-26 17:01:21 +0200

    first commit

---
+++ helpers.py
@@ -0,0 +1,44 @@
+# Helper functions
+
+import os
+import glob # library for loading images from a directory
+import matplotlib.image as mpimg
+import numpy as np
+
+
+
+# This function loads in images and their labels and places them in a list
+# The list contains all images and their associated labels
+# For example, after data is loaded, im_list[0][:] will be the first image-label pair in the list
+def load_dataset(image_dir):
+    
+    # Populate this empty image list
+    im_list = []
+    image_types = ["red", "yellow", "green"]
+    
+    # Iterate through each color folder
+    for im_type in image_types:
+        
+        # Iterate through each image file in each image_type folder
+        # glob reads in any image with the extension "image_dir/im_type/*"
+        for file in glob.glob(os.path.join(image_dir, im_type, "*")):
+            
+            # Read in the image
+            im = mpimg.imread(file)
+            
+            # Check if the image exists/if it's been correctly read-in
+            if not im is None:
+                # Append the image, and it's type (red, green, yellow) to the image list
+                im_list.append((im, im_type))
+
+    return im_list
+
+
+def crop_image(ori_image, row_crop=5, col_crop=3):
+    image_crop = np.copy(ori_image)
+    # Define how many pixels to slice off the sides of the original image
+    row_crop = row_crop
+    col_crop = col_crop
+    image_cropped = ori_image[row_crop:-row_crop, col_crop:-col_crop, :]
+    
+    return image_cropped
